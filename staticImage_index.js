7528902330d4a61e0233a1886bed764990fbb34e
mcgovey <kmcg82004@gmail.com>
2020-05-15 07:36:38 -0400

    added static image predictions

--- staticImage_index.js
+++ staticImage_index.js
@@ -1,48 +1,57 @@
-let net;
-const webcamElement = document.getElementById('webcam');
+
+
+// async function app() {
+//   console.log('Loading model..');
+
+//   // Load the model.
+//   // net = await mobilenet.load();
+//   const net = await tf.automl.loadImageClassification('model.json');
+//   console.log('Successfully loaded model');
+  
+//   // Create an object from Tensorflow.js data API which could capture image 
+//   // from the web camera as Tensor.
+//   const webcam = await tf.data.webcam(webcamElement);
+//   while (true) {
+//     const img = await webcam.capture();
+//     const result = await net.classify(img);
+
+//     console.log(result);
+//     document.getElementById('console').innerText = `
+//       prediction: ${result[0].label}\n
+//       probability: ${result[0].prob}
+//     `;
+//     // Dispose the tensor to release the memory.
+//     img.dispose();
+
+//     // Give some breathing room by waiting for the next animation frame to
+//     // fire.
+//     await tf.nextFrame();
+//   }
+// }
 
 async function app() {
-  console.log('Loading model..');
+	console.log('Loading model..');
+	const model = await tf.automl.loadImageClassification('model.json');
+	console.log('Successfully loaded model');
 
-  // Load the model.
-  // net = await mobilenet.load();
-  const net = await tf.automl.loadImageClassification('model.json');
-  console.log('Successfully loaded model');
-  
-  // Create an object from Tensorflow.js data API which could capture image 
-  // from the web camera as Tensor.
-  const webcam = await tf.data.webcam(webcamElement);
-  while (true) {
-    const img = await webcam.capture();
-    const result = await net.classify(img);
+	const imgs = ['img1', 'img2', 'img3', 'img4', 'img5'];
 
-    console.log(result);
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].label}\n
-      probability: ${result[0].prob}
-    `;
-    // Dispose the tensor to release the memory.
-    img.dispose();
+	for (let i = 0; i < imgs.length; i++) {
+		const element = imgs[i];
+		const image = document.getElementById(element);
+		const result = await model.classify(image);
+		console.log(result);
 
-    // Give some breathing room by waiting for the next animation frame to
-    // fire.
-    await tf.nextFrame();
-  }
+		// write output to div
+		document.getElementById(element+'_console').innerText = `
+				prediction: ${result[0].label}\n
+				probability: ${result[0].prob}\n
+				prediction: ${result[1].label}\n
+				probability: ${result[1].prob}
+			`;
+		
+	}
+	
 }
-
-// function readURL(input) {
-// 	if (input.files && input.files[0]) {
-// 			var reader = new FileReader();
-
-// 			reader.onload = function (e) {
-// 				img = document.getElementById('imageUploaded');
-// 				img.setAttribute('src', e.target.result);
-// 				img.width(250);
-// 				img.height(200);
-// 			};
-
-// 			reader.readAsDataURL(input.files[0]);
-// 	}
-// }
 
 app();
